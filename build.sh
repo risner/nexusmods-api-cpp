a3146c267157c50e9106c4984e44c15d9f0f0da4
James Risner <risner@lokkak.com>
2025-10-25 18:16:20 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rm -rf build && cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build --parallel
+rm -rf build CMakeCache.txt CMakeFiles && cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build --parallel
